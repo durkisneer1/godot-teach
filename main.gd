fcb81dc1f89ee829be63979eb571a3966f5f2dd4
durk <martinez.d048432@stu.sanjac.edu>
2024-05-02 02:18:41 -0500

    sumn like that

--- main.gd
+++ main.gd
@@ -1,7 +1,17 @@
-extends Node3D
+extends Node
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene_to_file("res://menu.tscn")
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().quit()
+	elif Input.is_action_just_pressed("ui_accept"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		elif Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- player/player.gd
+++ player/player.gd
@@ -1,35 +1,63 @@
 extends CharacterBody3D
 
-const SPEED = 4.0
-const JUMP_VELOCITY = 4.5
-const MOUSE_SENS = 0.001
-const MAX_ANGLE = deg_to_rad(89)
+
+const SPEED = 0.8
+const SPRINT = 2
+const MOUSE_SENS = 0.002
+const TURN_SPEED = 10
+const MAX_ROT_X = deg_to_rad(80)
+const MAX_TILT_ANGLE = deg_to_rad(2)
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var time_passed_since = 0.0
 
-@onready var head = $Head
+@onready var animation_player = $AnimationPlayer
+@onready var pivot = $Torso/Pivot
+@onready var camera = $Torso/Pivot/Camera
+@onready var body = $Body
+@onready var head = $Body/Head
+@onready var light = $Light
+@onready var torso = $Torso
+@onready var footsteps = $Footsteps
+@onready var flashlight = $Flashlight
 
 
-func _unhandled_input(event):
+func _input(event):
 	if event is InputEventMouseMotion:
-		var y_axis_rotation = event.relative.x * MOUSE_SENS
-		rotate_y(-y_axis_rotation)
+		body.rotate_y(-event.relative.x * MOUSE_SENS)
 		head.rotate_x(-event.relative.y * MOUSE_SENS)
-		head.rotation.x = clamp(head.rotation.x, -MAX_ANGLE, MAX_ANGLE)
+		head.rotation.x = clamp(head.rotation.x, -MAX_ROT_X, MAX_ROT_X)
+		light.rotation.y = body.rotation.y
+		light.rotation.x = head.rotation.x
 
 
 func _physics_process(delta):
+	time_passed_since += delta
+	
+	torso.rotation.y = lerp_angle(torso.rotation.y, body.rotation.y, TURN_SPEED * delta)
+	pivot.rotation.x = lerp_angle(pivot.rotation.x, head.rotation.x, TURN_SPEED * delta)
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
+	var direction = (body.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		var scalar = SPRINT if Input.is_action_pressed("sprint") else 1
+		velocity.x = direction.x * SPEED * scalar
+		velocity.z = direction.z * SPEED * scalar
+		
+		if animation_player.current_animation != "head_bob":
+			animation_player.play("head_bob")
+			
+		if time_passed_since > SPEED / scalar:
+			footsteps.play()
+			time_passed_since = 0.0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	pivot.rotation.z = lerp_angle(pivot.rotation.z, sign(input_dir.x) * -MAX_TILT_ANGLE, 0.05)
+	camera.rotation.x = lerp_angle(camera.rotation.x, sign(input_dir.y) * MAX_TILT_ANGLE, 0.05)
 
 	move_and_slide()
